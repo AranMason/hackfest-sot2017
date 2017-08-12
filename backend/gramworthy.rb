855b4f376d79ca361fdae2d92863102857747213
Samuel Wilson <samWson@users.noreply.github.com>
2017-08-12 13:20:03 +1200

    Add instagram oauth2

--- backend/gramworthy.rb
+++ backend/gramworthy.rb
@@ -39,3 +39,33 @@ get '/' do
 }.to_json]
 
 end
+
+=begin
+
+get "/media_search" do
+  client = Instagram.client(:access_token => session[:access_token])
+  html = "<h1>Get a list of media close to a given latitude and longitude</h1>"
+  for media_item in client.media_search("37.7808851","-122.3948632")
+    html << "<img src='#{media_item.images.thumbnail.url}'>"
+  end
+  html
+end
+
+Instagram.configure do |config|
+  config.client_id = "YOUR_CLIENT_ID"
+  config.client_secret = "YOUR_CLIENT_SECRET"
+  # For secured endpoints only
+  #config.client_ips = '<Comma separated list of IPs>'
+end
+
+get "/oauth/connect" do
+  redirect Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
+end
+
+get "/oauth/callback" do
+  response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
+  session[:access_token] = response.access_token
+  redirect "/nav"
+end
+
+=end

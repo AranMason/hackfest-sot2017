1adb37ec052881d2c0f6c6f366e9f413a8047f85
Samuel Wilson <samWson@users.noreply.github.com>
2017-08-13 11:00:56 +1200

    Add test code

--- gramworthy.rb
+++ gramworthy.rb
@@ -7,7 +7,7 @@ CALLBACK_URL 	= "https://gramworthy.herokuapp.com/oauth/callback"
 
 enable :sessions
 get '/' do
-	redirect '/oauth/connect'
+	redirect '/oauth/connect' # This needs to be changed back to root '/' later
 end
 
 get '/locations' do

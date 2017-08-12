5d86cf7c455b2949c842d45bdb7f9299ea81773e
Aran Mason <ang3l.0f.m3rcy@gmail.com>
2017-08-13 11:16:14 +1200

    fixes, again

--- gramworthy.rb
+++ gramworthy.rb
@@ -61,17 +61,7 @@ end
 get "/oauth/callback" do
   response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
   session[:access_token] = response.access_token
-    
-	client = Instagram.client(:access_token => sessions[:access_token])
-	html = "<h1>Get a list of media close to a given latitude and longitude</h1>"
-	
-	media_search(client.media_search("41.3139", "174.7694")
-	
-	client.media_search("-41.2770666667","-122.3948632").each do |media_item|
-		html << '<img src="#{media_item.images.thumbnail.url}">'
-	end
-	
-	html
+    redirect '/media_search'
 end
 
 
@@ -82,11 +72,8 @@ end
 get "/media_search" do
   client = Instagram.client(:access_token => session[:access_token])
   html = "<h1>Get a list of media close to a given latitude and longitude</h1>"
-
-  media_search(client.media_search("-41.2770666667","174.7784"))
-
-  #for media_item in client.media_search("-41.2770666667","-122.3948632")
-  #  html << "<img src='#{media_item.images.thumbnail.url}'>"
-  #end
-  #html
+  for media_item in client.media_search("37.7808851","-122.3948632")
+    html << "<img src='#{media_item.images.thumbnail.url}'>"
+  end
+  html
 end

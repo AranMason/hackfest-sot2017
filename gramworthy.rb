8a37035ea61a66a4b5dd373d0e9aabf3ac57866c
Samuel Wilson <samWson@users.noreply.github.com>
2017-08-13 11:25:34 +1200

    Media search test

--- gramworthy.rb
+++ gramworthy.rb
@@ -71,9 +71,10 @@ end
 
 get "/media_search" do
   client = Instagram.client(:access_token => session[:access_token])
-  html = "<h1>Get a list of media close to a given latitude and longitude</h1>"
-  for media_item in client.media_search("37.7808851","-122.3948632")
-    html << "<img src='#{media_item.images.thumbnail.url}'>"
-  end
-  html
+ # html = "<h1>Get a list of media close to a given latitude and longitude</h1>"
+  [200, client.media_search("37.7808851","-122.3948632")
+#  for media_item in client.media_search()
+   # html << "<img src='#{media_item.images.thumbnail.url}'>"
+ # end
+  #html
 end

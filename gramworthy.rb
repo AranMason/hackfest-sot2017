7008b1d7557064947c30e5699ed2a9a8715e85e3
Aran Mason <ang3l.0f.m3rcy@gmail.com>
2017-08-14 15:39:48 +1200

    removed distance argument

--- gramworthy.rb
+++ gramworthy.rb
@@ -26,8 +26,7 @@ get '/locations' do
 	puts params.inspect
    [200, get_locations(client.media_search(
 	  :lat => params[:lat],
-	  :lng => params[:long],
-	  :distance => 5000
+	  :lng => params[:long]
 	))]
 
 end

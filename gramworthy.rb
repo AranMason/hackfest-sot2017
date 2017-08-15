bc1ecf497a48d2e42729340c1a9dd041f6c0458b
Aran Mason <ang3l.0f.m3rcy@gmail.com>
2017-08-15 12:06:26 +1200

    Changed to offical api for testing

--- gramworthy.rb
+++ gramworthy.rb
@@ -1,53 +1,38 @@
 require 'sinatra'
-require 'instagram_api'
+require 'instagram'
 require_relative 'data_handler'
 require 'net/http'
 require 'uri'
 
-#CLIENT_ID 		= "7c84da3caa784119b3550d2370a5c2da"
-#CLIENT_SECRET 	= "cab9868fa7374c68bf32966ce93df1be"
-#CALLBACK_URL 	= "https://gramworthy.herokuapp.com/oauth/callback"
 
-client = Instagram.client(
-	  :client_id     => '7c84da3caa784119b3550d2370a5c2da',
-	  :client_secret => 'cab9868fa7374c68bf32966ce93df1be',
-	  :callback_url  => 'https://gramworthy.herokuapp.com/callback'
-	)
-
+##-----------------------------------------------
+## Static Variables
+##-----------------------------------------------
+CLIENT_ID 		= "7c84da3caa784119b3550d2370a5c2da"
+CLIENT_SECRET 	= "cab9868fa7374c68bf32966ce93df1be"
+CALLBACK_URL 	= "https://localhost:4567/callback" #"https://gramworthy.herokuapp.com/oauth/callback"
 
 enable :sessions
 
-@token
+@token = ""
+
+##-----------------------------------------------
+## Instagram API Setup
+##-----------------------------------------------
+
+Instagram.configure do |config|
+  config.client_id = CLIENT_ID
+  config.client_secret = CLIENT_SECRET
+  # For secured endpoints only
+  #config.client_ips = '<Comma separated list of IPs>'
+end
+
+##-----------------------------------------------
+## Webpage Serving
+##-----------------------------------------------
 
 get '/' do
 	redirect '/auth' # This needs to be changed back to root '/' later
-end
-
-get '/locations' do
-   [200, get_locations(client.media_search(
-	  :lat => params[:lat],
-	  :lng => params[:long],
-	  :distance => 5000
-	)).to_json]
-
-end
-
-get '/locations/:id' do |id|
-	token = client.get_access_token
-	#214818324
-	uri = URI("https://api.instagram.com/v1/locations/#{id}/media/recent?access_token=#{token}")
-	
-	req = Net::HTTP::Get.new(uri)
-	
-	res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme== 'https') do |http|
-		request = Net::HTTP::Get.new uri
-		
-		responce = http.request request
-		
-		[200. responce.inspect]
-	
-	end
-
 end
 
 ##-----------------------------------------------
@@ -55,12 +40,17 @@ end
 ##-----------------------------------------------
 
 get '/auth' do
-	redirect client.authorize_url + "&scope=public_content"
+	redirect Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
 end
 
 get "/callback" do
-	client.get_access_token(params[:code])
-	redirect '/index.html'
+	response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
+	session[:access_token] = response.access_token
+	puts "Debug Data"
+	puts responce.inspect
+	puts response.access_token.inspect
+	puts session.inspect
+	redirect "/test"
 end
 
 get '/test' do
@@ -81,5 +71,32 @@ get "/media_search" do
   puts @token
   client = client.user
   [200,  client]
+end
+
+get '/locations' do
+   [200, get_locations(client.media_search(
+	  :lat => params[:lat],
+	  :lng => params[:long],
+	  :distance => 5000
+	)).to_json]
+
+end
+
+get '/locations/:id' do |id|
+	token = client.get_access_token
+	#214818324
+	uri = URI("https://api.instagram.com/v1/locations/#{id}/media/recent?access_token=#{token}")
+	
+	#req = Net::HTTP::Get.new(uri)
+	
+	res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme== 'https') do |http|
+		request = Net::HTTP::Get.new uri
+		
+		responce = http.request request
+		
+		[200. responce.inspect]
+	
+	end
+
 end
 
